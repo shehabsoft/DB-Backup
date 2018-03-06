dc47c9ea23e3b68c52e9705a46849f39c6799e59
Shehab Tarek <shehabsoft94@gmail.com>
2018-03-06 11:03:28 +0200

    delete last Monthly Budget
    delete last Monthly Budget

--- deleteLastMonthlyBudget.sql
+++ deleteLastMonthlyBudget.sql
@@ -1,23 +1,17 @@
-
+﻿
 delete  from purchase_history where purchase_id in
 (
-select ID from homebudget.purchase where monthlybudget_id in(
-
-select Max(ID) from homebudget.monthly_budget
+select ID from homebudget.purchase where monthlybudget_id in(59
 ));
 
 
-delete   from homebudget.purchase where monthlybudget_id in(
-
-select Max(ID) from homebudget.monthly_budget);
+delete   from homebudget.purchase where monthlybudget_id in( 59);
 
 
 
-delete  from homebudget.monthly_budget_category  where monthlyBudget_ID in(
-select Max(ID) from homebudget.monthly_budget);
+delete  from homebudget.monthly_budget_category  where monthlyBudget_ID in(59);
 
-delete  from homebudget.monthly_budget where ID =58;
-
+delete  from homebudget.monthly_budget where ID =(59)
 
 
 
